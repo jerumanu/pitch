98855bb90e5b0d27dc765c1da752043887368609
jerumanu <emmanueljeru200@gmail.com>
2020-12-04 21:19:20 +0300

    add  class

--- manage.py
+++ manage.py
@@ -1,11 +1,17 @@
-from app import create_app ,dp 
+from app import dp 
 from app.models import user 
+from flask_migrate import Migrate ,MigrateCommand
+
+
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
 
 @manager.shell
-
-
 def make_shell_context():
-    return dict(app= app , db = db ,User = User)
+    return dict(app = app,db = db,User = User, Role = Role )
 
 
 if __name__ == '__main__':
